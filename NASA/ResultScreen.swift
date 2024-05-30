7ffb4687c462d5615a82a34282535991683ad6cd
geboboi <gabie.jelita@gmail.com>
2024-05-31 01:49:09 +0700

    intro screen all done

---
+++ NASA/ResultScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ResultScreen.swift
+//  NASA
+//
+//  Created by Gabriela Sihutomo on 31/05/24.
+//
+
+import SwiftUI
+
+struct ResultScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultScreen()
+}
